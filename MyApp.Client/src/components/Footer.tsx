e814ebb8ae70980d1e7e3434947a4750f7d35c56
Demis Bellot <demis.bellot@gmail.com>
2025-12-03 18:14:37 +0800

    Update docs link

--- MyApp.Client/src/components/Footer.tsx
+++ MyApp.Client/src/components/Footer.tsx
@@ -12,7 +12,7 @@ export default () => {
                         A ServiceStack Project
                     </h3>
                     <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
-                        <a href="https://docs.servicestack.net"
+                        <a href="https://react-templates.net/docs"
                            className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0">
                             Read Documentation
                         </a>
